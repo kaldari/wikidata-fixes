d1bffe334a09a70763e3a375da12510a39344186
Remko de Keijzer (Mbch331) <mbch331.wikipedia@gmail.com>
2016-09-18 22:24:33 +0200

    When removing statement also editinfo needs to be sent. And minor formatting updates Signed-off-by:Remko de Keijzer (Mbch331) <mbch331.wikipedia@gmail.com>

--- fix.php
+++ fix.php
@@ -1,6 +1,7 @@
 <?php
-error_reporting( E_ALL );
-ini_set( 'display_errors', 1 );
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 use Mediawiki\Api\MediawikiApi;
 use Mediawiki\Api\ApiUser;
@@ -19,20 +20,18 @@ use DataValues\StringValue;
 require_once( __DIR__ . '/vendor/autoload.php' );
 require_once( __DIR__ . '/config.inc.php' );
 
-date_default_timezone_set( 'UTC' );
+date_default_timezone_set('UTC');
 
 // Use the Wikidata API and Login
-$api = new MediawikiApi( 'https://www.wikidata.org/w/api.php' );
-$api->login( new ApiUser( $username, $password ) );
+$api = new MediawikiApi('https://www.wikidata.org/w/api.php');
+$api->login(new ApiUser($username, $password));
 
 // Set the bot flag for the edits
-$editInfo = new EditInfo( '', false, true );
+$editInfo = new EditInfo('', false, true);
 
 // Instantiate WikibaseFactory
 $services = new WikibaseFactory(
-    $api,
-    new DataValues\Deserializers\DataValueDeserializer(),
-    new DataValues\Serializers\DataValueSerializer()
+	$api, new DataValues\Deserializers\DataValueDeserializer(), new DataValues\Serializers\DataValueSerializer()
 );
 
 // Get specific services
@@ -42,42 +41,37 @@ $creator = $services->newStatementCreator();
 
 // Put list of items here
 $itemList = array(
-	'Q58984'
+    'Q58984'
 );
 
-foreach ( $itemList as $item ) {
-	$revision = $getter->getFromId( $item );
-	$itemData = $revision->getContent()->getData();
+foreach ($itemList as $item) {
+    $revision = $getter->getFromId($item);
+    $itemData = $revision->getContent()->getData();
 
-	// Get all the country claims for this item
-	$statementList = $itemData->getStatements();
-	$countryStatementList = $statementList->getByPropertyId( PropertyId::newFromNumber( 17 ) );
+    // Get all the country claims for this item
+    $statementList = $itemData->getStatements();
+    $countryStatementList = $statementList->getByPropertyId(PropertyId::newFromNumber(17));
 
-	// Remove existing country statements
-	foreach ( $countryStatementList as $countryStatement ) {
-		$remover->remove( $countryStatement );
-		sleep( 2 );
-	}
+    // Remove existing country statements
+    foreach ($countryStatementList as $countryStatement) {
+	$remover->remove($countryStatement, $editinfo);
+	sleep(2);
+    }
 
-	// Create new statement: country:novalue
-	$creator->create(
-		new PropertyNoValueSnak(
-			PropertyId::newFromNumber( 17 )
-		),
-		$item,
-		$editInfo
-	);
-	sleep( 10 );
+    // Create new statement: country:novalue
+    $creator->create(
+	    new PropertyNoValueSnak(
+	    PropertyId::newFromNumber(17)
+	    ), $item, $editInfo
+    );
+    sleep(10);
 
-	// Create new statement: located in the administrative territorial entity:Antarctic Treaty area
-	$creator->create(
-		new PropertyValueSnak(
-			PropertyId::newFromNumber( 131 ),
-			new EntityIdValue( new ItemId( 'Q21590062' ) )
-		),
-		$item,
-		$editInfo
-	);
-	echo $item . "\n";
-	sleep( 10 );
+    // Create new statement: located in the administrative territorial entity:Antarctic Treaty area
+    $creator->create(
+	    new PropertyValueSnak(
+	    PropertyId::newFromNumber(131), new EntityIdValue(new ItemId('Q21590062'))
+	    ), $item, $editInfo
+    );
+    echo $item . "\n";
+    sleep(10);
 }
